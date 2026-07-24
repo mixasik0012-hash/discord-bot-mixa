f4f77ad817086fa70fcbd5337470d6fc8d19c614
mixasik0012-hash <mixasik0012@gmail.com>
2026-07-24 12:08:14 +0300

    Update app.py

--- site/app.py
+++ site/app.py
@@ -187,5 +187,4 @@ def save_settings(guild_id):
     return redirect(url_for('dashboard', guild_id=guild_id))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
